e653299be6ac2be0bc6214bd2ce8805b05c9de09
Varun P <37121364+patelv712@users.noreply.github.com>
2019-05-25 18:40:38 -0400

    Create Background.java
    This is the file to create the background image for the JFrame window

---
+++ Background.java
@@ -0,0 +1,31 @@
+package Game;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Background extends JComponent
+	{
+		BufferedImage myImage = null;
+		private static final long serialVersionUID = 1L;
+		public void paintComponent(Graphics g)
+		{
+			try
+			{
+				myImage = ImageIO.read(new URL("https://media.graytvinc.com/images/810*456/ice-web.jpg")); //used to handle image data
+				super.paintComponent(g);
+				g.drawImage(myImage, 0, 0, this.getWidth(), this.getHeight(), this); //use this not null bc when method called first time image has not been loaded yet so error if null
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+	    }
+	}
+	
