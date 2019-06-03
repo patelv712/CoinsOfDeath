cc695b46ad85eb5e12946fe034bb9da1f06f8973
Varun P <37121364+patelv712@users.noreply.github.com>
2019-06-03 12:51:40 -0400

    Update Background.java

--- Background.java
+++ Background.java
@@ -1,11 +1,10 @@
-package Game;
-
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.net.URL;
@@ -13,7 +12,7 @@ import java.net.URL;
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
-public class Background extends JComponent
+public class Background extends JComponent 
 {
 
 	BufferedImage myImage = null;
@@ -22,15 +21,14 @@ public class Background extends JComponent
 	Graphics2D x = null;
 
 	Component a = new Button();
-	KeyEvent W = new KeyEvent(a,1,1,0, KeyEvent.VK_W, 'w' );
-	KeyEvent A = new KeyEvent(a,1,1,0, KeyEvent.VK_A, 'a' );
-	KeyEvent S = new KeyEvent(a,1,1,0, KeyEvent.VK_S, 's' );
-	KeyEvent D = new KeyEvent(a,1,1,0, KeyEvent.VK_D, 'd' );
 	
-	int xC = 700;
-	int yC = 350;
-	int h = 150;
-	int w = 150;
+	private int xC = 700;
+	private int yC = 350;
+	public Background()
+	{
+		//addKeyListener(this);
+		//setFocusable(true);
+	}
 	
 	private static final long serialVersionUID = 1L;
 
@@ -45,8 +43,8 @@ public class Background extends JComponent
 			x.setColor(Color.GREEN); //just in case background is still there
 			//x.fillOval(700, 400, 100, 100);
 			guy = ImageIO.read(new URL("https://images-na.ssl-images-amazon.com/images/I/61O3LuvpDML._SY355_.png"));
-			x.drawImage(guy,xC,yC, h, w,  new Color(0,0,0,0), y);
-			keyPress(A);
+			x.drawImage(guy,xC,yC, 150, 150,  new Color(0,0,0,0), y);
+			
 			
 		}
 		catch(Exception e)
@@ -54,40 +52,27 @@ public class Background extends JComponent
 			e.printStackTrace();
 		}
     }
-	public void keyPress(KeyEvent evt)
+	
+	public void moveUp()
 	{
-		
-		int key = evt.getKeyCode();
-		switch(key)
-		{
-			case KeyEvent.VK_W:
-			{
-				yC--;
-				System.out.println("" + evt.getKeyChar());  //for testing purposes
-				break;
-			}
-			case KeyEvent.VK_A:
-			{
-				xC--;
-				break;
-			}
-			case KeyEvent.VK_S:
-			{
-				yC++;
-				break;
-			}
-			case KeyEvent.VK_D:
-			{
-				xC++;
-				break;
-			}
-			default:
-			{
-				System.out.println("bruhhhh");
-				break;
-			}
-		}
-		x.drawImage(guy,xC,yC, h, w,  new Color(0,0,0,0), y)	;
+		yC+= 10;
+		repaint();
 	}
+	public void moveRight()
+	{
+		xC+= 10;
+		repaint();
+	}
+	public void moveLeft()
+	{
+		xC -= 10;
+		repaint();
+	}
+	public void moveDown()
+	{
+		yC -= 10;
+		repaint();
+	}
+	
 	
 }
