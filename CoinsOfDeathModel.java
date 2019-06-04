51c9a86bcbf8a502dc0a844ec4afd1d257828bae
Varun P <37121364+patelv712@users.noreply.github.com>
2019-06-04 16:23:55 -0400

    Manages the game as a whole

---
+++ CoinsOfDeathModel.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public final class CoinsOfDeathModel {
+    private TimeLimit time;
+    private ArrayList<Coin> points ;
+    private Guy player;
+    private int score;
+}
