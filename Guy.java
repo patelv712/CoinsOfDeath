3527c4efe470745165faea1cf9183e5b6cb599e3
Varun P <37121364+patelv712@users.noreply.github.com>
2019-06-04 16:23:09 -0400

    Manages the player in the game

---
+++ Guy.java
@@ -0,0 +1,22 @@
+public final class Guy {
+    private double width;
+    private double height;
+    private double x;
+    private double y;
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+    
+    public double getX() {
+        return this.x;
+    }
+    
+    public double getY() {
+        return this.y;
+    }
+}
