0e3deb85f23f429e9e0685429b07a57609d38278
Varun P <37121364+patelv712@users.noreply.github.com>
2019-06-04 16:22:39 -0400

    Create Coin.java
    Manages the coins in the game

---
+++ Coin.java
@@ -0,0 +1,25 @@
+public final class Coin {
+    private double width;
+    private double height;
+    private double x;
+    private double y;
+    //private boolean hasCollided;
+     public double getWidth() {
+        return this.width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+    
+    public double getX() {
+        return this.x;
+    }
+    
+    public double getY() {
+        return this.y;
+    }
+/* public boolean getCollide()
+{
+    return this.hasCollided;
+}   */
