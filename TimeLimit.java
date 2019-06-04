f381d95950a0dde66cba923a575e934f951f0f00
Varun P <37121364+patelv712@users.noreply.github.com>
2019-06-04 16:25:10 -0400

    Manages the time in the game

---
+++ TimeLimit.java
@@ -0,0 +1,14 @@
+public final class TimeLimit
+{
+    java.time.Duration timeRemaining;
+    java.time.Duration timeTotal;
+    
+    public java.time.Duration getRemaining()
+    {
+      return timeRemaining;
+    }
+    public java.time.Duration getTotal()
+    {
+      return timeTotal;
+    }
+}
