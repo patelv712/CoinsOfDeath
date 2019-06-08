10cd0fb1f7eed2cd118bbdce892c92a40f568bc1
Ethan McCue <emccue@live.com>
2019-06-08 16:21:01 -0700

    Update TimeLimit.java

--- TimeLimit.java
+++ TimeLimit.java
@@ -1,12 +1,12 @@
 import java.time.Duration;
 public final class TimeLimit
 {
-    java.time.Duration timeRemaining;
-    java.time.Duration timeTotal;
+    private java.time.Duration timeRemaining;
+    private java.time.Duration timeTotal;
 
-    public TimeLimit(java.time.Duration timeTotal) 
+    public TimeLimit(java.time.Duration timeTotal)
     {
-        if (timeTotal == null) 
+        if (timeTotal == null)
         {
             throw new IllegalArgumentException("Total time should not be null");
         }
@@ -19,27 +19,29 @@ public final class TimeLimit
     }
     public boolean timeUp()
     {
-    	if (this.timeRemaining == Duration.ZERO) 
-    	{
-    		return true;
-    	}
-    	return false;
+        if (this.timeRemaining == Duration.ZERO)
+        {
+            return true;
+        }
+        return false;
     }
     public void decreaseTime(long amount)
     {
-    	if (this.timeRemaining.minus(Duration.ofMillis(amount)).isNegative())
-    	{
-    		this.timeRemaining = Duration.ZERO;    
-    	}
-    	this.timeRemaining = this.timeRemaining.minus(Duration.ofMillis(amount));
+        if (this.timeRemaining.minus(Duration.ofMillis(amount)).isNegative())
+        {
+            this.timeRemaining = Duration.ZERO;
+        }
+        else {
+            this.timeRemaining = this.timeRemaining.minus(Duration.ofMillis(amount));
+        }
     }
     public java.time.Duration getRemaining()
     {
-      return this.timeRemaining;
+        return this.timeRemaining;
     }
 
     public java.time.Duration getTotal()
     {
-      return this.timeTotal;
+        return this.timeTotal;
     }
 }
