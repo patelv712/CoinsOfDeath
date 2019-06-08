d9097015aff3641b76e2fbfb59508b4fe2dbdcb9
Ethan McCue <emccue@live.com>
2019-06-08 16:20:44 -0700

    Update Guy.java

--- Guy.java
+++ Guy.java
@@ -1,42 +1,45 @@
-public final class Guy 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public final class Guy
 {
     private double width;
     private double height;
     private double x;
     private double y;
-   
+
     public Guy(double width, double height, double x, double y)
     {
-       	if (height <= 0 || width <= 0)
-    	{
-    		throw new IllegalArgumentException("Height/Width should not be negative/Zero !");
-    	}
-    	this.width = width;
-    	this.height = height;
-    	this.x = x;
-    	this.y = y;
+        if (height <= 0 || width <= 0)
+        {
+            throw new IllegalArgumentException("Height/Width should not be negative/Zero !");
+        }
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
     }
     public void moveUp(double amount)
-	{
-		this.y -= amount;
-	}
-	
-	public void moveRight(double amount)
-	{
-		this.x += amount;
-	}
-	
-	public void moveLeft(double amount)
-	{
-		this.x -= amount;
-	}
-	
-	public void moveDown(double amount)
-	{
-		this.y +=amount;
-	}
-	
-    public double getWidth() 
+    {
+        this.y -= amount;
+    }
+
+    public void moveRight(double amount)
+    {
+        this.x += amount;
+    }
+
+    public void moveLeft(double amount)
+    {
+        this.x -= amount;
+    }
+
+    public void moveDown(double amount)
+    {
+        this.y +=amount;
+    }
+
+    public double getWidth()
     {
         return this.width;
     }
@@ -45,14 +48,20 @@ public final class Guy
     {
         return this.height;
     }
-    
-    public double getX() 
+
+    public double getX()
     {
         return this.x;
     }
-    
-    public double getY() 
+
+    public double getY()
     {
         return this.y;
+    }
+
+    public boolean isColliding(Coin coin) {
+        Rectangle2D thisShape = new java.awt.geom.Rectangle2D.Double(this.x, this.y, this.width, this.height);
+        Rectangle2D otherShape = new java.awt.geom.Rectangle2D.Double(coin.getX(), coin.getY(), coin.getWidth(), coin.getHeight());
+        return thisShape.intersects(otherShape);
     }
 }
